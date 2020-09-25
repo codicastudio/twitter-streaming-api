d1bfb0ab6d82640e49f4fd978f71e665fb03f313
codica-studio <CodicaStudioDev@gmail.com>
2020-09-24 21:40:38 -0500

    Initial

---
+++ tests/ExampleTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Spatie\TwitterStreamingApi\Test;
+
+class ExampleTest extends \PHPUnit_Framework_TestCase
+{
+    /** @test */
+    public function true_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}
